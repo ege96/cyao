8265a4583114ca844d47fd3a5509622acbe7b770
Eric <eric.ge96@gmail.com>
2024-02-19 15:41:59 -0500

    added CORS

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user.router)
